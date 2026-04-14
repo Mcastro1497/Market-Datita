ddb4a3ea3f6d0f16f1f614530697afd21aafee34
Mcastro1497 <manucastro9714@gmail.com>
2026-04-13 21:36:10 -0300

    Fix filteredEmisores mapping to filter out falsy values

--- components/soberanos-ars-details-filters.tsx
+++ components/soberanos-ars-details-filters.tsx
@@ -184,7 +184,7 @@ export function SoberanosArsDetailsFilters({ monedas, emisores, onFiltersChange 
                   </div>
                 </div>
                 <div className="space-y-2 max-h-60 overflow-y-auto">
-                  {filteredEmisores.map((emisor) => (
+                  {filteredEmisores.filter(Boolean).map((emisor) => (
                     <div key={emisor} className="flex items-center space-x-2">
                       <Checkbox
                         id={emisor}
